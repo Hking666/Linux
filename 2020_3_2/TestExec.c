1878b399eae4cb2f0987ec28a90f3d83c6cb535d
Hking666 <1358263860@qq.com>
2020-03-02 22:09:09 +0800

    2020_3_2

---
+++ 2020_3_2/TestExec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+int main()
+{
+
+    char *const argv[] = {"ps","ef",NULL};
+    char *const envp[] = {"PATH=/bin:/usr/bin","TERM=console",NULL};
+
+    execl("/bin/ps","ps","-ef",NULL);
+
+    execlp("ps","ps","-ef",NULL);
+
+    execle("ps","ps","-ef",NULL);
+
+    execv("/bin/ps",argv);
+
+    execvp("ps",argv);
+
+    execve("/bin/ps",argv,envp);
+    
+    exit(0);
+}
